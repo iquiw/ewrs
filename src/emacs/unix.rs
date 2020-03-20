aa6a288e3d2dc7462f600941a978d64f34166fd0
Iku Iwasa <iku.iwasa@gmail.com>
2020-03-20 10:52:54 +0900

    Fix emacs server socket path for XDG_RUNTIME_DIR
    It does not contain UID in the path.
    
    Also use dirs crate instead of using environment variable directly.

--- src/emacs/unix.rs
+++ src/emacs/unix.rs
@@ -2,6 +2,7 @@ use std::ffi::OsStr;
 use std::io::{stderr, Error, ErrorKind, Result, Write};
 use std::path::{Path, PathBuf};
 
+use dirs::runtime_dir;
 use libc::{fork, getuid, setsid};
 
 use super::common::Emacs;
@@ -10,6 +11,32 @@ const EMACS_CMD: &'static str = "emacs";
 const EMACSCLI_CMD: &'static str = "emacsclient";
 
 pub struct UnixEmacs {}
+
+impl UnixEmacs {
+    #[cfg(feature = "emacs27")]
+    fn emacs_server_dir() -> PathBuf {
+        if let Some(dir) = runtime_dir() {
+            let mut path = PathBuf::from(dir);
+            path.push("emacs");
+            path
+        } else {
+            let mut path = PathBuf::from("/tmp");
+            unsafe {
+                path.push(format!("emacs{}", getuid()));
+            }
+            path
+        }
+    }
+
+    #[cfg(not(feature = "emacs27"))]
+    fn emacs_server_dir() -> PathBuf {
+        let mut path = PathBuf::from("/tmp");
+        unsafe {
+            path.push(format!("emacs{}", getuid()));
+        }
+        path
+    }
+}
 
 impl Emacs for UnixEmacs {
     fn new() -> Self {
@@ -21,18 +48,7 @@ impl Emacs for UnixEmacs {
     }
 
     fn is_server_running(&self) -> Option<PathBuf> {
-        #[cfg(feature = "emacs27")]
-        let mut path = if let Some(dir) = std::env::var_os("XDG_RUNTIME_DIR") {
-            PathBuf::from(dir)
-        } else {
-            PathBuf::from("/tmp")
-        };
-        #[cfg(not(feature = "emacs27"))]
-        let mut path = PathBuf::from("/tmp");
-
-        unsafe {
-            path.push(format!("emacs{}", getuid()));
-        }
+        let mut path = UnixEmacs::emacs_server_dir();
         path.push("server");
         if path.exists() {
             Some(PathBuf::from(EMACSCLI_CMD))
